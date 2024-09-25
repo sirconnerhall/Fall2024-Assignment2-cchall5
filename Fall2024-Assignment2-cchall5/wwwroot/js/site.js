9d988fb89e35650c185282837b0c24f5dc0e7f27
chall2022 <cchall5@crimson.ua.edu>
2024-09-25 12:15:55 -0500

    added files from blackboard

--- Fall2024-Assignment2-cchall5/wwwroot/js/site.js
+++ Fall2024-Assignment2-cchall5/wwwroot/js/site.js
@@ -1,5 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function apiSearch() {
+    var params = {
+        'q': $('#query').val(),
+        'count': 50,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
 
-// Write your JavaScript code.
+    $.ajax({
+        url: 'https://MY-API-ENDPOINT/v7.0/search?' + $.param(params),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': 'MY-API-KEY'
+        }
+    })
+        .done(function (data) {
+            var len = data.webPages.value.length;
+            var results = '';
+            for (i = 0; i < len; i++) {
+                results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
+            }
 
+            $('#searchResults').html(results);
+            $('#searchResults').dialog();
+        })
+        .fail(function () {
+            alert('error');
+        });
+}
