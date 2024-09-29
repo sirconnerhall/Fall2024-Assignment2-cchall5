c00aa397186e1bdfb047ac468c2dfd02075b1792
chall2022 <cchall5@crimson.ua.edu>
2024-09-29 17:28:28 -0500

    adding images, buttons, and divs

--- Fall2024-Assignment2-cchall5/wwwroot/js/site.js
+++ Fall2024-Assignment2-cchall5/wwwroot/js/site.js
@@ -7,10 +7,10 @@
     };
 
     $.ajax({
-        url: 'https://MY-API-ENDPOINT/v7.0/search?' + $.param(params),
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
         type: 'GET',
         headers: {
-            'Ocp-Apim-Subscription-Key': 'MY-API-KEY'
+            'Ocp-Apim-Subscription-Key': '3c892121a2b64339aaf0d5c890acdc37'
         }
     })
         .done(function (data) {
@@ -26,4 +26,32 @@
         .fail(function () {
             alert('error');
         });
+}
+
+// call API search when search button is clicked
+document.getElementById("searchButton").addEventListener("click", function () {
+    apiSearch();
+});
+
+// array of background images
+const backgroundImages = [
+    '/images/fall_pond.jpg',
+    '/images/fall_railroad.jpeg',
+    '/images/fall_road.jpeg',
+    '/images/fall_trees.jpeg',
+    '/images/fall_tunnel.jpeg'
+]
+// set default
+document.body.style.backgroundImage = `url('${backgroundImages[0]}')`;
+
+// change background image when search engine name clicked
+document.getElementById("engineName").addEventListener("click", function () {
+    const idx = Math.floor(Math.random() * backgroundImages.length);
+    const imgUrl = backgroundImages[idx];
+    document.body.style.backgroundImage = `url('${imgUrl}')`;
+})
+
+// get/display current time on click of time button
+function displayTime() {
+
 }
